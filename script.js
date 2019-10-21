e7227305ae8dd42aa87c5816a038de692c9d183d
BRENDA C CARDONA <52057830+BrendaCardona@users.noreply.github.com>
2019-10-21 13:52:41 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+
 // array of quotes
 var quotes = [
 	{
 	quote: 'If debugging is the process of removing software bugs, then programming must be process of putting them in.',
-	source: '–Edsger Dijkstra'
+	source: '–Edsger Dijkstra',
+	citation: 'reddit.com'
 	},
 	{
 	quote: 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.',
@@ -22,39 +24,23 @@ var quotes = [
 	{
 	quote: 'Before software can be reusable it first has to be usable.',
 	source: '– Ralph Johnson',
-	citation: 'softwarenature.com',
-	year: '2016'
+	citation: 'softwarenature.com'
   },
   {
 	quote: 'Without requirements or design, programming is the art of adding bugs to an empty text file.',
 	source: '–Louis Srygley',
 	citation: 'Twitter'
-  },
-  {
-	quote: 'Every great developer you know got there by solving problems they were unquialified to solve until they actually did it.',
-	source: 'Patrick McKenzie.',
-	citation: 'Twitter',
-	year: '2016'
-	}
+  }
 ];
 
-// create a viewedQuotes variable
-var viewedQuotes = [];
-
 //Create the 'getRandomQuote' function //
-function getRandomQuote() {
+function getRandomQuote(array) {
 	var quoteIndex = Math.floor( Math.random() * (quotes.length));
-	// splice adds/removes items to/from an array and return removed items
-		var splicedQuote = quotes.splice(quoteIndex, 1)[0];
-			viewedQuotes.push(splicedQuote);
-		
-		if (quotes.length === 0){
-		quotes = viewedQuotes;
-		viewedQuotes = [];
-		}
-	return splicedQuote;
-		}
-		
+	for (var i = 0; i < array.length; i++ ) {
+		var randomQuote = array[quoteIndex];
+}
+return randomQuote;
+}
  // create function for random colors
 setInterval(function() {
 	var red = Math.round(Math.random() * 255 );
@@ -68,16 +54,11 @@ setInterval(function() {
 
 // function to print quotes on the screen
 function printQuote() {
-	var message = "";
+	var message = '';
 	var randomQuote = getRandomQuote(quotes);
   message = "<p class='quote'>" + randomQuote.quote + "</p>";
   message += "<p class='source'>" + randomQuote.source;
-	if (randomQuote.citation) {
-		message += '<span class="citation">' + randomQuote.citation + '</span>';
-	}
-	if (randomQuote.year) {
-		message += '<span class="year">' + randomQuote.year + '</span>';
-	}
+  message += "<span class='citation'>" + randomQuote.citation + "</span>";
   message += "</p>";
 
     document.getElementById('quote-box').innerHTML = message;
@@ -86,6 +67,3 @@ function printQuote() {
 printQuote();
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-// call the printQuote to  update the quote periodically
-setInterval(printQuote, 4000); 
